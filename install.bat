9fefb01afdbe6616e7d8badcdf83e18eb4b7fe01
Roelf-Jilling <r-j.wolthuis@live.com>
2018-01-28 03:56:24 +0100

    =comment-edit

--- install.bat
+++ install.bat
@@ -13,15 +13,13 @@
 ::
 ::===----------------------------------------------------------------------===::
 @echo off
+pushd "%~dp0" &REM Set current directory to the location of this batch file.
 
 echo Installing MSVC integration...
-set /a "_SCount=0"
-set /a "_FCount=0"
+set /a "_SuccessCnt=0"
+set /a "_FailCnt=0"
 
-REM Set current directory to the location of this batch file.
-pushd "%~dp0"
-
-:: Legacy installations.
+:: Legacy installations. By explicit directory structure.
 if defined ProgramFiles(x86) call :fn_legacy "%ProgramFiles(x86)%"
 if not %ERRORLEVEL%==0 goto FINISHED
 call :fn_legacy "%ProgramFiles%"
@@ -29,17 +27,17 @@ if not %ERRORLEVEL%==0 goto FINISHED
 :: VS2017 (VC++ toolset v141) and later.
 call :fn_vswhere
 
-:: Status reports.
 :FINISHED
 if not %ERRORLEVEL%==0 echo ERROR: Internal script error.
-if not %_FCount%==0 (
-	echo WARNING: Copy operation failed for %_FCount% installations.
+if not %_FailCnt%==0 (
+	echo WARNING: Copy operation failed for %_FailCnt% installations.
 	echo:         Verify write access. (Run as administrator.^)
 )
-if %_SCount%==0 ( echo WARNING: Failed to install any toolset.
-) else ( echo Installed integation for %_SCount% toolsets. )
+if %_SuccessCnt%==0 ( echo WARNING: Failed to install any toolset.
+) else ( echo Installed integation for %_SuccessCnt% toolsets. )
 echo Done!
-popd
+
+popd &REM Reset current directory.
 exit /b
 
 ::===----------------------------------------------------------------------===::
@@ -59,12 +57,12 @@ call :fn_platforms "%_BaseDir%\MSBuild\Microsoft.Cpp\v4.0\V110"
 call :fn_platforms "%_BaseDir%\MSBuild\Microsoft.Cpp\v4.0\V120"
 :: VS2015 (v140).
 call :fn_platforms "%_BaseDir%\MSBuild\Microsoft.Cpp\v4.0\V140"
-endlocal & set "_SCount=%_SCount%" & set "_FCount=%_FCount%"
+endlocal & set "_SuccessCnt=%_SuccessCnt%" & set "_FailCnt=%_FailCnt%"
 goto:eof
 
 
 :fn_vswhere
-:: Install integration since VS2017. Using the Visual Studio Locator tool.
+:: Find MSVC toolsets since VS2017 with the Visual Studio Locator tool.
 setlocal
 if DEFINED ProgramFiles(x86) (
   set "_Vswhere=%ProgramFiles(x86)%") else (set "_Vswhere=%ProgramFiles%")
@@ -72,10 +70,10 @@ set _Vswhere="%_Vswhere%\Microsoft Visual Studio\Installer\vswhere.exe"
 if not exist %_Vswhere% goto:eof
 for /f "usebackq tokens=*" %%i in (
   `%_Vswhere% -all -prerelease -products * -property installationPath`
-) do ( :: Construct path equal to $(VCTargetsPath).
+) do ( REM Construct path equal to $(VCTargetsPath).
   call :fn_platforms "%%i\Common7\IDE\VC\VCTargets"
 )
-endlocal & set "_SCount=%_SCount%" & set "_FCount=%_FCount%"
+endlocal & set "_SuccessCnt=%_SuccessCnt%" & set "_FailCnt=%_FailCnt%"
 goto:eof
 
 
@@ -90,13 +88,13 @@ for /f "usebackq tokens=*" %%P in (`dir "!_VCTargetsPath!\Platforms" /a:d /b`
 ) do (
   if exist ".\%%P" (
     call :fn_toolsets "!_VCTargetsPath!\Platforms\%%P\PlatformToolsets" %%P
-    if not !ERRORLEVEL!==0 set /a "_FCount+=1"
+    if not !ERRORLEVEL!==0 set /a "_FailCnt+=1"
   )
 )
-endlocal & ( REM /EnableDelayedExpansion.
-  set "_SCount=%_SCount%" & set "_FCount=%_FCount%" )
-endlocal & set "_SCount=%_SCount%" & set "_FCount=%_FCount%"
-exit /b 0 &:: Contain ERRORLEVEL.
+endlocal & ( REM /EnableDelayedExpansion
+  set "_SuccessCnt=%_SuccessCnt%" & set "_FailCnt=%_FailCnt%" )
+endlocal & set "_SuccessCnt=%_SuccessCnt%" & set "_FailCnt=%_FailCnt%"
+exit /b 0 &REM Contain ERRORLEVEL.
 
 
 :fn_toolsets
@@ -105,14 +103,15 @@ setlocal DisableDelayedExpansion
 if [%2]==[] echo DEBUG: fn_toolsets - no input & goto:eof
 set "_ToolsetDir=%~1"
 set "_Platform=%2"
-:: -------------- configurations --------------
+::===------------ configurations ------------------------------------------===::
 :: Installing the v100 toolchain.
 set   _MSname=v100           &REM Default toolset folder.
 set _LLVMname=LLVM-vs2010    &REM New folder.
 set    _Props=Microsoft.Cpp.%_Platform%.LLVM-vs2010.props
 set  _Targets=Microsoft.Cpp.%_Platform%.LLVM-vs2010.targets
 if exist "%_ToolsetDir%\%_MSname%" (
-  call :fn_copy "%_ToolsetDir%" %_LLVMname% %_Platform% %_Props% %_Targets% "doNotRename")
+  call :fn_copy ^
+    "%_ToolsetDir%" %_LLVMname% %_Platform% %_Props% %_Targets% "doNotRename")
 if not %ERRORLEVEL%==0 goto:eof
 
 :: Installing the v110 toolchain.
@@ -121,14 +120,17 @@ set _LLVMname=LLVM-vs2012
 set    _Props=Microsoft.Cpp.%_Platform%.LLVM-vs2012.props
 set  _Targets=Microsoft.Cpp.%_Platform%.LLVM-vs2012.targets
 if exist "%_ToolsetDir%\%_MSname%" (
-  call :fn_copy "%_ToolsetDir%" %_LLVMname% %_Platform% %_Props% %_Targets% "doNotRename")
+  call :fn_copy ^
+    "%_ToolsetDir%" %_LLVMname% %_Platform% %_Props% %_Targets% "doNotRename")
 if not %ERRORLEVEL%==0 goto:eof
+:: Installing the v110_xp toolchain.
 set   _MSname=v110_xp
 set _LLVMname=Microsoft.Cpp.%_Platform%.LLVM-vs2012_xp
 set    _Props=Microsoft.Cpp.%_Platform%.LLVM-vs2012_xp.props
 set  _Targets=Microsoft.Cpp.%_Platform%.LLVM-vs2012_xp.targets
 if exist "%_ToolsetDir%\%_MSname%" (
-  call :fn_copy "%_ToolsetDir%" %_LLVMname% %_Platform% %_Props% %_Targets% "doNotRename")
+  call :fn_copy ^
+    "%_ToolsetDir%" %_LLVMname% %_Platform% %_Props% %_Targets% "doNotRename")
 if not %ERRORLEVEL%==0 goto:eof
 
 :: Installing the v120 toolchain.
@@ -139,6 +141,7 @@ set  _Targets=toolset-vs2013.targets
 if exist "%_ToolsetDir%\%_MSname%" (
   call :fn_copy "%_ToolsetDir%" %_LLVMname% %_Platform% %_Props% %_Targets%)
 if not %ERRORLEVEL%==0 goto:eof
+:: Installing the v120_xp toolchain.
 set   _MSname=v120_xp
 set _LLVMname=LLVM-vs2013_xp
 set    _Props=toolset-vs2013_xp.props
@@ -155,6 +158,7 @@ set  _Targets=toolset-vs2014.targets
 if exist "%_ToolsetDir%\%_MSname%" (
   call :fn_copy "%_ToolsetDir%" %_LLVMname% %_Platform% %_Props% %_Targets%)
 if not %ERRORLEVEL%==0 goto:eof
+:: Installing the v140_xp toolchain.
 set   _MSname=v140_xp
 set _LLVMname=LLVM-vs2014_xp
 set    _Props=toolset-vs2014_xp.props
@@ -164,13 +168,14 @@ if exist "%_ToolsetDir%\%_MSname%" (
 if not %ERRORLEVEL%==0 goto:eof
 
 :: Installing the v141 toolchain.
-set   _MSname=v141           &REM Default toolset folder.
-set _LLVMname=LLVM-vs2017    &REM New folder.
+set   _MSname=v141        &REM Default toolset folder.
+set _LLVMname=LLVM-vs2017 &REM New folder.
 set    _Props=toolset-vs2017.props
 set  _Targets=toolset-vs2017.targets
 if exist "%_ToolsetDir%\%_MSname%" (
   call :fn_copy "%_ToolsetDir%" %_LLVMname% %_Platform% %_Props% %_Targets%)
 if not %ERRORLEVEL%==0 goto:eof
+:: Installing the v141_xp toolchain.
 set   _MSname=v141_xp
 set _LLVMname=LLVM-vs2017_xp
 set    _Props=toolset-vs2017_xp.props
@@ -178,18 +183,19 @@ set  _Targets=toolset-vs2017_xp.targets
 if exist "%_ToolsetDir%\%_MSname%" (
   call :fn_copy "%_ToolsetDir%" %_LLVMname% %_Platform% %_Props% %_Targets%)
 if not %ERRORLEVEL%==0 goto:eof
-:: --------------------------------------------
-endlocal & set "_SCount=%_SCount%"
+::===----------------------------------------------------------------------===::
+endlocal & set "_SuccessCnt=%_SuccessCnt%"
 goto:eof
 
 
 :fn_copy
 :: Perform the copy operations.
 setlocal
-:: arguments: 1 ToolsetDir, 2 Folder, 3 platform 4 .props, 5 .targets, 6 doNotRename
-if [%5]==[] echo DEBUG: fn_copy - no input & goto:eof
+:: Arguments:
+::   1 ToolsetDir, 2 Folder, 3 platform 4 .props, 5 .targets, 6 doNotRename
+if [%5]==[]     echo DEBUG: fn_copy - no input    & goto:eof
 if not exist %1 echo DEBUG: fn_copy - input error & goto:eof
-if not exist ".\%3\%4" goto:eof &:: No LLVM toolset configuration defined.
+if not exist ".\%3\%4" goto:eof &REM No LLVM toolset configuration defined.
 echo Install: %2 (%3)
 set "_Dir=%~1\%2"
 if not exist "%_Dir%" mkdir "%_Dir%" & echo make dir
@@ -200,11 +206,11 @@ if not [%6]==[] goto doNotRename
   if exist ".\%3\%5" copy %3\%5 "%_Dir%\toolset.targets" > NUL
   if not %ERRORLEVEL%==0 goto:eof
 goto CopyEnd
-:doNotRename &:: VS2010 & VS2012.
+:doNotRename &REM VS2010 and VS2012.
   if exist ".\%3\%4" copy %3\%4 "%_Dir%" > NUL
   if not %ERRORLEVEL%==0 goto:eof
   if exist ".\%3\%5" copy %3\%5 "%_Dir%" > NUL
   if not %ERRORLEVEL%==0 goto:eof
 :CopyEnd
-endlocal & set /a "_SCount+=1"
+endlocal & set /a "_SuccessCnt+=1"
 goto:eof
--- uninstall.bat
+++ uninstall.bat
@@ -13,20 +13,18 @@
 ::
 ::===----------------------------------------------------------------------===::
 @echo off
+pushd "%~dp0" &REM Set current directory to the location of this batch file.
 
 echo Uninstalling MSVC integration...
 
-REM Set current directory to the location of this batch file.
-pushd "%~dp0"
-
-:: Legacy installations.
+:: Legacy installations. By explicit directory structure.
 if defined ProgramFiles(x86) call :fn_legacy "%ProgramFiles(x86)%"
 call :fn_legacy "%ProgramFiles%"
 :: VS2017 (VC++ toolset v141) and later.
 call :fn_vswhere
 
 echo Done!
-popd
+popd &REM Reset current directory.
 exit /b
 
 ::===----------------------------------------------------------------------===::
@@ -51,7 +49,7 @@ goto:eof
 
 
 :fn_vswhere
-:: Remove integration since VS2017. Using the Visual Studio Locator tool.
+:: Find MSVC toolsets since VS2017 with the Visual Studio Locator tool.
 setlocal
 if DEFINED ProgramFiles(x86) (
   set "_VSWhere=%ProgramFiles(x86)%") else (set "_VSWhere=%ProgramFiles%")
@@ -59,7 +57,7 @@ set _VSWhere="%_VSWhere%\Microsoft Visual Studio\Installer\vswhere.exe"
 if not exist %_VSWhere% goto:eof
 for /f "usebackq tokens=*" %%i in (
   `%_VSWhere% -all -prerelease -products * -property installationPath`
-) do ( :: Construct path equal to $(VCTargetsPath).
+) do ( REM Construct path equal to $(VCTargetsPath).
   call :fn_platforms "%%i\Common7\IDE\VC\VCTargets"
 )
 endlocal
@@ -83,7 +81,7 @@ goto:eof
 
 
 :fn_remove
-:: Remove integrations that follow the LLVM naming format.
+:: Remove toolset configurations that follow the LLVM naming format.
 setlocal
 if [%1]==[] echo DEBUG: fn_remove: no input & goto:eof
 set "_ToolsetDir=%~1"
@@ -96,7 +94,7 @@ for /f "usebackq tokens=*" %%D in (`dir "%_ToolsetDir%\LLVM-*" /a:d /b`) do (
   if exist !_File! del !_File!
   set _File="%_ToolsetDir%\%%D\toolset.targets"
   if exist !_File! del !_File!
-  :: For VS2010 / VS2012.
+  :: For VS2010 & VS2012.
   set _File="%_ToolsetDir%\%%D\Microsoft.Cpp.%_Platform%.LLVM-vs201*.props"
   if exist !_File! del !_File!
   set _File="%_ToolsetDir%\%%D\Microsoft.Cpp.%_Platform%.LLVM-vs201*.targets"
